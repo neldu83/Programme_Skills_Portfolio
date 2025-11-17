7b294fc8397d0ed21952f250515711f5653c8912
neldu83 <2409-123195@esl.ac.uk>
2025-11-17 11:43:12 +0000

    Add files via upload

---
+++ 01- Coding is cool/CodingIsCool.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Provides cout and endl for printing text
+#include <string> // Includes in the templates, though not requires for this task
+
+using namespace std; // Allows using cout without writing std: :cout
+
+int main() {
+    cout << "coding is cool" <<endl; // print the message followed by a new line.
+
+    return 0; // End program successfully.
+    
+}
