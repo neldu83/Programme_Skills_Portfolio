34bd7cd52b7a51a7ff1cb0c6b4489c0adcbf03c3
neldu83 <2409-123195@esl.ac.uk>
2025-11-17 12:19:07 +0000

    Add files via upload

---
+++ 02- Simple Sums/SimpleSums.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+
+int main() { 
+    int a = 8;       // first variable
+    int b = 10;      // second variable 
+    int sum = a + b; // third variable
+
+    cout << sum << endl; // print the sum 
+
+    return 0;
+}   
