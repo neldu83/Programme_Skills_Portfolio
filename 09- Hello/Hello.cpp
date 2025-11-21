8b6b19a4cf03ecda7d18c4c62d6ccfe121a410cc
neldu83 <2409-123195@esl.ac.uk>
2025-11-21 15:52:38 +0000

    Add files via upload

--- 09- Hello/Hello.cpp
+++ 09- Hello/Hello.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-using namespace std;
-
-void hello (){
-    cout << "Hello" << endl;
+#include <iostream>   // Includes the library for input/output (cout, endl)
+using namespace std;  // Allows using standard library names without "std::"
+void hello (){    // Defines a function named `hello1` that takes no parameters
+    cout << "Hello" << endl; // Prints "Hello" followed by a newline 
 }
 
-int main(){
+int main(){   // Start of the main function (program entry point) 
 
-    hello();
+    hello();  // Calls the `hello` function to output "Hello"
 
-    return 0;
+    return 0;  // Returns 0 to indicate successful program execution 
     
 }
