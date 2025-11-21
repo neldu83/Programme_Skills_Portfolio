a104160b72ef67f594427a4ad496b06743ffb140
neldu83 <2409-123195@esl.ac.uk>
2025-11-21 15:42:51 +0000

    Add files via upload

--- 02- Simple Sums/SimpleSums.cpp
+++ 02- Simple Sums/SimpleSums.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-using namespace std; 
+#include <iostream> // Includes the input and output library (cout, endl)
+using namespace std;  // Allows using standard names without writting std::
 
-int main() { 
-    int a = 8;       // first variable
-    int b = 10;      // second variable 
-    int sum = a + b; // third variable
+int main() {         // Start of the main function (progarm entry point)
+    int a = 8;       // first integer variable assigned to value 8
+    int b = 10;      // second integer variable assigned to value 10
+    int sum = a + b; // third  variable that stores the sum of a and b 
 
-    cout << sum << endl; // print the sum 
+    cout << sum << endl; // print the value stored in `sum` followed by a new line 
 
-    return 0;
+    return 0;  // Return to 0 to indicate successful program execution 
 }   
