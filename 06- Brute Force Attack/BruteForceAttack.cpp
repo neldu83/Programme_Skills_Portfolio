6b83677605da3c51b3d79809c685ece2614bb005
neldu83 <2409-123195@esl.ac.uk>
2025-11-21 15:48:34 +0000

    Add files via upload

--- 06- Brute Force Attack/BruteForceAttack.cpp
+++ 06- Brute Force Attack/BruteForceAttack.cpp
@@ -1,30 +1,31 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <iostream>  // Includes input/output library (cout, cin)
+#include <string>   // Includes string library for std::string
+using namespace std;  // Allows using standard library names without "std::"
 
 int main() {
     const string correctPassword = "12345"; // required by the task\n
-    string attempt;
-    int attemptsLeft = 5;
+    string attempt;   // Variable to store user`s input 
+    int attemptsLeft = 5;  // User has 5 attempts before lockout 
 
-    cout << "Brute Force Attack\n";
+    cout << "Brute Force Attack\n";  // Display program title
 
-    while (attemptsLeft > 0) { 
-        cout << "Enter Password (" << attemptsLeft << " attempts left): ";
-        cin >> attempt;
+    // Loop continues while the user still has attempts left
+    while (attemptsLeft > 0) {  
+        cout << "Enter Password (" << attemptsLeft << " attempts left): ";  // Shows how many attempts remain
+        cin >> attempt;  // Read password attempt from user 
  
-        if (attempt ==correctPassword) {
-        cout << "Welcome to the Secure areal!" << endl;
-        return 0; //  end program when correct
-    } else {
-        attemptsLeft--;
-        if (attemptsLeft > 0) {
+        if (attempt ==correctPassword) { // check if the attempt matches the real password 
+        cout << "Welcome to the Secure areal!" << endl; // Success message
+        return 0; //  End program when correct
+    } else {      // If password is wrong:
+        attemptsLeft--;  // Reduce remaining attempts by 1
+        if (attemptsLeft > 0) {  // If user still has attempts keft:
             cout << "Incorrect password. Try again." << endl;
         }
     }
 }
-        
+// This message only appears when attempts left reaches 0      
 cout << "Too many failed attempts. The authorities have been alerted!" << endl;
-return 0;
+return 0;  // Program Ends
 
 }
